47002f8bc26a5530afa87951c22edab187e6edbf
Andreas Ntalakas <antalakas@gmail.com>
2017-05-14 23:25:07 +0300

    clears the stage, adds datasets to gitignore

--- cli.py
+++ cli.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-import findlane
 from findlane.findlane.calibratecamera import CalibrateCamera
+from findlane.findlane.findlane import FindLane
+from detection.detection.detection import Detection
 import os
 
 
@@ -16,15 +17,17 @@ def dependencies():
 
 
 #///////////////////////////////////////////////////
-#  FindLane command line interface
-def findlane_cli():
+#  Vehicle Detection command line interface
+def detection_cli():
     # ///////////////////////////////////////////////////
     parser = argparse.ArgumentParser(
-        description="findlane_cli - A lightweight command-line wrapper for advanced lane finding.")
+        description="detection_cli - A lightweight command-line wrapper for Vehicle Detection.")
 
     parser.add_argument('--cal', action='store', dest='cal', help='Performs camera calibration')
-    parser.add_argument('--imgf', action='store', dest='img', help='Executes findlane pipeline for test images')
-    parser.add_argument('--vidf', action='store', dest='vid', help='Executes findlane pipeline for project video')
+    parser.add_argument('--imgf', action='store', dest='imgf', help='Executes findlane pipeline for test images')
+    parser.add_argument('--vidf', action='store', dest='vidf', help='Executes findlane pipeline for project video')
+    parser.add_argument('--imgd', action='store', dest='imgd', help='Executes detection pipeline for test images')
+    parser.add_argument('--vidd', action='store', dest='vidd', help='Executes detection pipeline for project video')
 
     # ///////////////////////////////////////////////////
     args = parser.parse_args()
@@ -37,19 +40,27 @@ def findlane_cli():
         print("successfully calibrated: %s images" % str(successfully_calibrated))
         cc.check_undistort(False)
 
-    if args.img:
+    if args.imgf:
         fl = FindLane(base_directory)
         fl.execute_image_pipeline(True)
 
-    if args.vid:
+    if args.vidf:
         fl = FindLane(base_directory)
         fl.project_video()
+
+    if args.imgd:
+        det = Detection(base_directory)
+        det.execute_image_pipeline(True)
+
+    if args.vidd:
+        det = Detection(base_directory)
+        det.project_video()
 
 if __name__ == "__main__":
 
     try:
         if dependencies():
-            findlane_cli()
+            detection_cli()
         else:
             raise(Exception, "Packages required: ...")
     except KeyboardInterrupt:
